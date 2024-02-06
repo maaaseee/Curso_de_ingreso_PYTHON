a28ce0096d3534cb70b0d1c7fe2cffc6ffe1b6c4
Federico Aieta <maxe1111@hotmail.com>
2024-02-06 11:16:08 -0300

    Resolvi el Ej 01 y 02

--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_02.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_02.py
@@ -27,7 +27,9 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        #snake case
+        nombre_alumno = "Federico"
+        alert("Datos" , nombre_alumno)
         
         
     
